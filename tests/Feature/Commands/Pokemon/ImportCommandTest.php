6b8472654f5f2c4b09ec06a816a63af184f0a307
Josh <josh@bonnick.dev>
2024-05-17 17:18:59 +0100

    prevent stray request in test

--- tests/Feature/Commands/Pokemon/ImportCommandTest.php
+++ tests/Feature/Commands/Pokemon/ImportCommandTest.php
@@ -4,8 +4,12 @@ namespace Tests\Feature\Commands\Pokemon;
 
 use App\Console\Commands\Pokemon\ImportCommand;
 use App\Jobs\Pokemon\ImportPokemon;
+use App\Services\PokeAPI\Contracts\PokeAPIClient;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Queue;
+use Mockery;
+use Mockery\MockInterface;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -14,6 +18,12 @@ class ImportCommandTest extends TestCase
     #[Test]
     public function it_queues_import_job(): void
     {
+        Http::preventStrayRequests();
+
+        $this->instance(PokeAPIClient::class, Mockery::mock(PokeAPIClient::class, function (MockInterface $mock) {
+            $mock->shouldReceive('listPokemon')->andReturn(['results' => [['url' => '::fake::']]]);
+        }));
+
         Queue::fake();
 
         Artisan::call(ImportCommand::class);
