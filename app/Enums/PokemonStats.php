1e9f4d02011d45a021f9cf8dcd1761fb533e51ec
Josh <josh@bonnick.dev>
2024-05-18 02:16:02 +0100

    bugs: fix special attack/defence sorting

--- app/Enums/PokemonStats.php
+++ app/Enums/PokemonStats.php
@@ -8,6 +8,8 @@ enum PokemonStats: string
     case SPEED = 'speed';
     case ATTACK = 'attack';
     case DEFENSE = 'defense';
+    case SPECIAL_ATTACK = 'special_attack';
+    case SPECIAL_DEFENSE = 'special_defense';
 
     /**
      * @return array<string, string>
@@ -17,6 +19,8 @@ enum PokemonStats: string
         return collect(self::cases())->mapWithKeys(fn (self $enum) => [
             $enum->value => match ($enum) {
                 self::HP => 'HP',
+                self::SPECIAL_ATTACK => 'Special Attack',
+                self::SPECIAL_DEFENSE => 'Special Defense',
                 default => ucfirst($enum->value)
             },
         ])->toArray();
